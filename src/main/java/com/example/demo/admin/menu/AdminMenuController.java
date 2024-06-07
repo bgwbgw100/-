3743883a9b760f68c78c6fb8ba85b4e4f5cc0abb
back <bgwbgw1001@gmail.com>
2024-06-07 10:55:17 +0900

    menu 완료

--- src/main/java/com/example/demo/admin/menu/AdminMenuController.java
+++ src/main/java/com/example/demo/admin/menu/AdminMenuController.java
@@ -15,9 +15,7 @@ import java.util.List;
 @Controller
 @RequestMapping("admin")
 @RequiredArgsConstructor
-public class
-
-AdminMenuController {
+public class AdminMenuController {
 
     private final AdminMenuService adminMenuService;
 
